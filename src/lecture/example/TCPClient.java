db02562101e6a34cc2db5f5be21e1141da115701
Dries Janse <djse@live.be>
2019-03-04 11:50:04 +0100

    Example from slides

---
+++ src/lecture/example/TCPClient.java
@@ -0,0 +1,31 @@
+package lecture.example;
+
+import java.io.*;
+import java.net.*;
+
+public class TCPClient {
+
+	
+	public static void main(String[] args) throws Exception {
+		//read from input
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		// host/port to connect to
+		Socket clientSocket = new Socket("localhost",9876);
+		
+		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		
+		//read server data
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		
+		String sentence = inFromUser.readLine();
+		
+		outToServer.writeBytes(sentence + '\n');
+		
+		String modifiedSentence = inFromServer.readLine();
+		
+		System.out.println("FROM SERVER: " + modifiedSentence);
+		
+		clientSocket.close();
+		
+	}
+}
