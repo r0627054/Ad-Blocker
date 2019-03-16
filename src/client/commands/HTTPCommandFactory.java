760e991c606a7a1de358ec3ab0b43f3b010d3600
Dries Janse <djse@live.be>
2019-03-17 00:58:59 +0100

    HTTPCommandFactory doc

--- src/client/commands/HTTPCommandFactory.java
+++ src/client/commands/HTTPCommandFactory.java
@@ -1,9 +1,27 @@
 package client.commands;
 
 import httpproperties.HTTPMethod;
-
+/**
+ * HTTPCommandFactory is a class which uses the factory design pattern.
+ * It creates the corresponding httpCommand using the accompanied httpMethod.
+ *   The httpCommand is initiated with the given uriString and port.
+ *
+ */
 public class HTTPCommandFactory {
 
+	/**
+	 * Creates the corresponding httpCommand using the accompanied httpMethod.
+     *   The httpCommand is initiated with the given uriString and port.
+     *   By default a HTTPGetCommand is used.
+	 * @param httpMethod
+	 *        | the HTTP Method used
+	 * @param uriString
+	 *        | the uri given in string format
+	 * @param port
+	 *        | the port used by the socket
+	 * @return the HTTP Command created
+	 * @throws IllegalArgumentException if the given method equals null
+	 */
 	public static HTTPCommand getHTTPCommand(HTTPMethod httpMethod, String uriString, int port) {
 		if(httpMethod == null) {
 			throw new IllegalArgumentException("Cannot create a HTTPCommand when httpMethod is null.");
