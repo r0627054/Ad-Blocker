e25609bde6eec8fd228e37a1b902070c397a3a21
Dries Janse <djse@live.be>
2019-03-17 00:47:08 +0100

    HTTP 1.1 client doc

--- src/client/HTTP11Client.java
+++ src/client/HTTP11Client.java
@@ -6,13 +6,28 @@ import client.commands.HTTPCommand;
 import client.handlers.ClientRequestHandler;
 import client.handlers.ClientRequestHandlerFactory;
 
-
+/**
+ * The HTTP11Client is a subclass of a HTTPClient.
+ *   The client makes use of the version 1.1 of HTTP.
+ *
+ */
 public class HTTP11Client extends HTTPClient {
 
+	/**
+	 * Initialises the HTTPClient
+	 */
 	public HTTP11Client() {
 		
 	}
 	
+	/**
+	 * Handles the given command.
+	 * It handles the command by requesting the correct handler,
+	 * the handler needs to handle the command further.
+	 * @param command
+	 *        | the http command
+	 * @throws Exception
+	 */
 	@Override
 	public void handleCommand(HTTPCommand command) throws Exception {
 		if(this.socket == null) {
