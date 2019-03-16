8d825e9585ac7eb56ceb573363e25aeabd3b2da6
Dries Janse <djse@live.be>
2019-03-17 00:46:50 +0100

    HTTP Client doc

--- src/client/HTTPClient.java
+++ src/client/HTTPClient.java
@@ -1,17 +1,31 @@
 package client;
 
-import java.io.IOException;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 import client.commands.HTTPCommand;
 
+/**
+ * Every HTTP client should have a socket and will need to handle a command.
+ *
+ */
 public abstract class HTTPClient {
 
+	/**
+	 * Variable storing the socket.
+	 */
 	protected Socket socket;
 	
+	/**
+	 * Handles the given command
+	 * @param command
+	 *        | the http command
+	 * @throws Exception
+	 */
 	public abstract void handleCommand(HTTPCommand command) throws Exception;
 
+	/**
+	 * Returns the socket of the client.
+	 */
 	public Socket getSocket() {
 		return this.socket;
 	}
