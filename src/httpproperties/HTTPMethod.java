a1ed1877aca658388f846aa5a82db93fd04b24d7
Dries Janse <djse@live.be>
2019-03-17 01:58:25 +0100

    HTTP version + method doc

--- src/httpproperties/HTTPMethod.java
+++ src/httpproperties/HTTPMethod.java
@@ -1,17 +1,33 @@
 package httpproperties;
 
+/**
+ * The HTTP methods supported in the client.
+ *  It stores the method and stores the capital String of the method (methodcode).
+ *
+ */
 public enum HTTPMethod {
 	HEAD("HEAD"),
 	GET("GET"),
 	PUT("PUT"),
 	POST("POST");
 	
+	/**
+	 * The variable storing the HTTP method code (capital letters)
+	 */
 	private final String methodCode;
 	
+	/**
+	 * Initialises the HTTPMethod with the method code
+	 * @param methodCode
+	 *        | the method code of the http method
+	 */
 	private HTTPMethod(String methodCode) {
 		this.methodCode = methodCode;
 	}
 	
+	/**
+	 * Returns the method code of the of the HTTPMETHOD
+	 */
 	public String getMethodCode() {
 		return this.methodCode;
 	}
--- src/httpproperties/HTTPVersion.java
+++ src/httpproperties/HTTPVersion.java
@@ -1,14 +1,28 @@
 package httpproperties;
 
+/**
+ * Stores the different HTTP versions which can be used.
+ */
 public enum HTTPVersion {
 HTTP10("HTTP/1.0"), HTTP11("HTTP/1.1");
 	
+	/**
+	 * Variable storing the version code: the version in the correct String format.
+	 */
 	private final String versionCode;
 	
+	/**
+	 * Initialise the HTTPVersion with the correct versioncode.
+	 * @param versionCode
+	 *        | the versioncode of the HTTPVersion
+	 */
 	private HTTPVersion(String versionCode) {
 		this.versionCode = versionCode;
 	}
 	
+	/**
+	 * Returns the version code of a HTTPVersion.
+	 */
 	public String getVersionCode() {
 		return this.versionCode;
 	}
