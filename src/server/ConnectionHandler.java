cedee0c5b99139a9db21dc7c1b7b3bc8dd44e115
Mauro Luyten <mauro.luyten@gmail.com>
2019-03-17 11:15:05 +0100

    reworked connectionhandler

--- src/server/ConnectionHandler.java
+++ src/server/ConnectionHandler.java
@@ -1,5 +1,7 @@
 package server;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
@@ -43,31 +45,72 @@ public class ConnectionHandler implements Runnable{
 	 */
 	@Override
 	public void run() {
-		try {
-			HTTPRequestHeader header = new HTTPRequestHeader(helperHandler.getHeaderString(clientSocket.getInputStream()));
-			
-			byte[] body = null;
-			
-			if(header.getContentLength()>=0) {
-				body = helperHandler.getBytes(header.getContentLength(), clientSocket.getInputStream());			
-			}
-			
-			HTTPMethod method = HTTPMethod.valueOf(header.getHttpMethod());
-			String uriString = header.getHeaderValue("Host") + header.getHeaderValue("Path");
-			HTTPCommand command = HTTPCommandFactory.getHTTPCommand(method, uriString, clientSocket.getLocalPort());
-			
-			ServerRequestHandler requestHandler = ServerRequestHandlerFactory.getHandler(command.getHttpmethod());
-			requestHandler.handle(command, body, header, clientSocket);
-			
-		} catch (Exception e) {
-			try {
-				helperHandler.respond404(clientSocket);
-			} catch (IOException e1) {
+		try (
+		        BufferedInputStream bis = new BufferedInputStream(clientSocket.getInputStream());
+		        BufferedOutputStream bos = new BufferedOutputStream(clientSocket.getOutputStream());) {
+
+		        boolean alive = true;
+		        while (alive) {
+		            alive = false;
+		            try {
+		                HTTPRequestHeader header = null;
+		                try {
+		                	header = new HTTPRequestHeader(helperHandler.getHeaderString(bis));
+		                	
+		                	byte[] body = null;
+		                	
+		        			if(header.getContentLength()>=0) {
+		        				body = helperHandler.getBytes(header.getContentLength(), bis);			
+		        			}
+		        			
+		        			HTTPMethod method = HTTPMethod.valueOf(header.getHttpMethod());
+		        			String uriString = header.getHeaderValue("Host") + header.getHeaderValue("Path");
+		        			HTTPCommand command = HTTPCommandFactory.getHTTPCommand(method, uriString, clientSocket.getLocalPort());
+		        			
+		        			ServerRequestHandler requestHandler = ServerRequestHandlerFactory.getHandler(command.getHttpmethod());
+		        			requestHandler.handle(command, body, header, clientSocket);
+		                }catch (Exception e) {
+							helperHandler.respond404(clientSocket);
+						}
+		         
+		                alive = true;                    
+		            } catch (Exception e) {
+		                alive = false;
+		            }
+		        }
+		    } catch (IOException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
-			
-		}
+		
+		
+		
+		
+//		try {
+//			HTTPRequestHeader header = new HTTPRequestHeader(helperHandler.getHeaderString(clientSocket.getInputStream()));
+//			
+//			byte[] body = null;
+//			
+//			if(header.getContentLength()>=0) {
+//				body = helperHandler.getBytes(header.getContentLength(), clientSocket.getInputStream());			
+//			}
+//			
+//			HTTPMethod method = HTTPMethod.valueOf(header.getHttpMethod());
+//			String uriString = header.getHeaderValue("Host") + header.getHeaderValue("Path");
+//			HTTPCommand command = HTTPCommandFactory.getHTTPCommand(method, uriString, clientSocket.getLocalPort());
+//			
+//			ServerRequestHandler requestHandler = ServerRequestHandlerFactory.getHandler(command.getHttpmethod());
+//			requestHandler.handle(command, body, header, clientSocket);
+//			
+//		} catch (Exception e) {
+//			try {
+//				helperHandler.respond404(clientSocket);
+//			} catch (IOException e1) {
+//				// TODO Auto-generated catch block
+//				e1.printStackTrace();
+//			}
+//			
+//		}
 	}
 	
 
