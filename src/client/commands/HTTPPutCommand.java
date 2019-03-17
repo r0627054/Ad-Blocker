34938928b73513e6dfb25794b08b944a378db4c2
Dries Janse <djse@live.be>
2019-03-17 01:14:09 +0100

    http Put command doc

--- src/client/commands/HTTPPutCommand.java
+++ src/client/commands/HTTPPutCommand.java
@@ -5,24 +5,48 @@ import httpproperties.HTTPMethod;
 /**
  * The HTTP PUT request method creates a new resource or
  *  replaces a representation of the target resource with the request payload.
+ *  
+ * The HTTPPUTCommand is a subclass of the HTTPCommand.
+ *  This class is specifically made for the PUT REQUEST.
  *
  */
 public class HTTPPutCommand extends HTTPCommand {
 
-	
+	/**
+	 * Initialises a HTTP PUT command with the given variables.
+	 * @param uriString
+	 *        | the uri given in string format
+	 * @param port
+	 *        | the port which needs to be connected with
+	 * @param httpVersionString
+	 *        | the http version 
+	 */
 	public HTTPPutCommand(String uriString, int port, String httpVersionString) {
 		super(uriString,port,httpVersionString);
 	}
 	
+	/**
+	 * Initialises a HTTP PUT command with the given variables.
+	 * @param uriString
+	 *        | the uri given in string format
+	 * @param port
+	 *        | the port which needs to be connected with
+	 */
 	public HTTPPutCommand(String uriString, int port) {
 		super(uriString, port);
 	}
 
+	/**
+	 * Returns the HTTPMethod: PUT
+	 */
 	@Override
 	public HTTPMethod getHttpmethod() {
 		return HTTPMethod.PUT;
 	}
 
+	/**
+	 * Returns the generated header String in the correct format
+	 */
 	@Override
 	public String getHeader() {
 		String result = "";
@@ -31,6 +55,15 @@ public class HTTPPutCommand extends HTTPCommand {
 		return result;
 	}
 	
+	/**
+	 * Returns the generated header String in the correct format.
+	 * 
+	 * @param contentType
+	 *        | The type of the content used in the body of the post request
+	 * @param contentLength
+	 *        | the length of the body of the post request.
+	 * @return the fully created string, of the header.
+	 */
 	public String getFullHeader(String contentType, int contentLength) {
 		String result = "";
 		result += "PUT " + this.getPath() + " HTTP/1.1" + "\n";
