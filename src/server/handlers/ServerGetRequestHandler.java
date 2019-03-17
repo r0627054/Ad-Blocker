a20be3857aca95ec869d4f0f3a6a31f7456e9f07
Dries Janse <djse@live.be>
2019-03-17 15:11:20 +0100

    ServerGetRequestHandler doc

--- src/server/handlers/ServerGetRequestHandler.java
+++ src/server/handlers/ServerGetRequestHandler.java
@@ -1,24 +1,38 @@
 package server.handlers;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.file.AccessDeniedException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.text.SimpleDateFormat;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
 import java.util.Date;
 
 import client.commands.HTTPCommand;
 import server.HTTPRequestHeader;
 
+/**
+ * The ServerGetRequestHandler is a ServerRequestHandler which handles the GET request on the server side.
+ */
 public class ServerGetRequestHandler extends ServerRequestHandler {
 
+	/**
+	 * It handles the given GET command,
+	 *  If the resource is not found it responds with a 404 response.
+	 *  If the is was NOT modified since the date mentioned it responds with a code 304.
+	 *  If it was found it returns the resouce with a 200 found message.
+	 *  
+	 * @param command
+	 *        | the incoming get command which will be handled
+	 * @param body
+	 *        | the body of the incoming http request
+	 * @param header
+	 *        | the HTTPRequestHeader of the request
+	 * @param socket
+	 *        | the socket used to handle the command
+	 * @throws Exception
+	 */
 	@Override
 	public void handle(HTTPCommand command, byte[] body,HTTPRequestHeader header, Socket socket) throws Exception {
 		File fileToServe = new File("resources/server" + command.getPath());
@@ -44,6 +58,14 @@ public class ServerGetRequestHandler extends ServerRequestHandler {
 		}
 	}
 
+	/**
+	 * The actual 200 response of a get message. It writes the header and the actual file to the outputStream.
+	 * @param socket
+	 *        | the socket used to send the response
+	 * @param fileToServe
+	 *        | the file which is requested.
+	 * @throws IOException
+	 */
 	private void respond200(Socket socket, File fileToServe) throws IOException {
 		System.out.println("found " + fileToServe.getPath().toString());
 		String headerString = "HTTP/1.1 200 OK\n";
