0401b9667607929266ede7ca3233e9e2214733a4
Dries Janse <djse@live.be>
2019-03-17 01:02:42 +0100

    HTTPGetCommand

--- src/client/commands/HTTPGetCommand.java
+++ src/client/commands/HTTPGetCommand.java
@@ -1,22 +1,48 @@
 package client.commands;
 
 import httpproperties.HTTPMethod;
-
+/**
+ * The HTTPGetCommand is a subclass of the HTTPCommand.
+ *  This class is specifically made the GET REQUEST.
+ *
+ */
 public class HTTPGetCommand extends HTTPCommand {
 
+	/**
+	 * Initialises a HTTP command with the given variables.
+	 * @param uriString
+	 *        | the uri given in string format
+	 * @param port
+	 *        | the port which needs to be connected with
+	 * @param httpVersionString
+	 *        | the http version 
+	 */
 	public HTTPGetCommand(String uriString, int port, String httpVersionString) {
 		super(uriString,port,httpVersionString);
 	}
 	
+	/**
+	 * Initialises a HTTP command with the given variables.
+	 * @param uriString
+	 *        | the uri given in string format
+	 * @param port
+	 *        | the port which needs to be connected with
+	 */
 	public HTTPGetCommand(String uriString, int port) {
 		super(uriString, port);
 	}
 
+	/**
+	 * Returns the HTTPMethod: GET
+	 */
 	@Override
 	public HTTPMethod getHttpmethod() {
 		return HTTPMethod.GET;
 	}
 
+	/**
+	 * Returns the generated header String in the correct format.
+	 */
 	@Override
 	public String getHeader() {
 		String result = "";
