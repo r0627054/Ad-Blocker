bec8c8af7210cdb695ab7306762e0cf1edd2fce5
Dries Janse <djse@live.be>
2019-03-17 16:38:14 +0100

    RequestHandler Doc + reponse headers with date and contentlength refactor

--- src/shared/handlers/HttpRequestHandler.java
+++ src/shared/handlers/HttpRequestHandler.java
@@ -16,13 +16,30 @@ import client.commands.HTTPCommand;
 import client.commands.HTTPCommandFactory;
 import files.FileSaver;
 import httpproperties.HTTPMethod;
-
+/**
+ * The httpRequestHandler class is a class which has often
+ *  used methods of the clientRequestHandler and the ServerRequestHandler.
+ *
+ */
 public class HttpRequestHandler {
 	
+	/**
+	 * Initialises a new Instance of a HttpRequestHandler.
+	 * It initialises it without any parameters.
+	 */
 	public HttpRequestHandler() {
 		// TODO Auto-generated constructor stub
 	}
 	
+	/**
+	 * Reads the requested amount of bytes and returns it in a byte array.
+	 * @param numberOfBytes
+	 *        | the amount of bytes which needs to be read from the inputStream.
+	 * @param inputStream
+	 *        | the inputStream of the current socket.
+	 * @return the requested amount of bytes of the inputStream.
+	 * @throws IOException
+	 */
 	public byte[] getBytes(int numberOfBytes, InputStream inputStream) throws IOException {
 		//used with content-length is given
 		byte[] result =new byte[numberOfBytes];
@@ -32,10 +49,23 @@ public class HttpRequestHandler {
 		return result;
 	}
 	
+	/**
+	 * Returns String representation of the byte array.
+	 * @param bytes
+	 *        | the bytes which needs to be converted to a String
+	 * @return
+	 */
 	public String bytesToString(byte[] bytes) {
 		return new String(bytes);
 	}
 		
+	/**
+	 * Reads the header out of the inputStream.
+	 * @param inputStream
+	 *        | the inpuStream which received a request/response.
+	 * @return the header of the request/response in a String.
+	 * @throws IOException
+	 */
 	public String getHeaderString(InputStream inputStream) throws IOException {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		List<Integer> data = new ArrayList<>();
@@ -52,6 +82,13 @@ public class HttpRequestHandler {
 		return out.toString("UTF-8");
 	}
 	
+	/**
+	 * Reads a fully chuncked message.
+	 * @param inputStream
+	 *        | the inputStream which has chuncked data.
+	 * @return the array of bytes extracted from the chuncked message.
+	 * @throws Exception
+	 */
 	public byte[] readChunks(InputStream inputStream) throws Exception {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		
@@ -68,6 +105,19 @@ public class HttpRequestHandler {
 	}
 	
 
+	/**
+	 * Handles a Response body which the client received from the server.
+	 * @param command
+	 *        | the command which sended a request to the server.
+	 * @param inputStream
+	 *        | the inputStream on which the response is received.
+	 * @param header
+	 *        | the HTTP header of the response.
+	 * @param save
+	 *        | Whether or not the response needs to be saved.
+	 * @return The content of the body as a byte array.
+	 * @throws Exception
+	 */
 	protected byte[] handleOneRequest(HTTPCommand command, InputStream inputStream, HTTPHeader header, boolean save) throws Exception{
 		byte[] content = null;
 		if(header.containsHeader("Content-Length")) {
@@ -89,15 +139,40 @@ public class HttpRequestHandler {
 		return content;
 	}
 	
+	/**
+	 * Saves the content received from a server in a folder.
+	 * @param content
+	 *        | the content which needs to be saved.
+	 * @param host
+	 *        | the host of which the content was received (this will be the folder name)
+	 * @param filename
+	 *        | the filename which was received form the server.
+	 * @param filetype
+	 *        | The type of the file, this will be type of the file which will be saved.
+	 */
 	public void saveFile(byte[] content, String host, String filename, String filetype) {
 		new FileSaver( content,  host, filename, filetype);
 	}
 	
+	/**
+	 * Returns the the amount of bytes the next chunk will have.
+	 * @param inputStream
+	 *        | the inputStream on which the chunks are send.
+	 * @return the amount of bytes of the next chunk.
+	 * @throws Exception
+	 */
 	public int getLengthOfChunk(InputStream inputStream) throws Exception { 
 		String hexAmountOfBytes = bytesToString(this.readChunksLine(inputStream));
 		return this.hexaDecimalToInteger(hexAmountOfBytes);
 	}
 	
+	/**
+	 * Reads a line of the chunked data until a (13 10) CRLF. 
+	 * @param inputStream
+	 *        | the inputStream on which the chunked data is received.
+	 * @return the byte array of that line of the chunk.
+	 * @throws IOException
+	 */
 	public byte[] readChunksLine(InputStream inputStream) throws IOException {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		int currentByte;
@@ -120,6 +195,12 @@ public class HttpRequestHandler {
 		return out.toByteArray();
 	}
 		
+	/**
+	 * Converts a hexadecimal number to an Integer.
+	 * @param hex
+	 *        | the hexaDecimal number represented as a String.
+	 * @return the Integer representation of the hexadecimal number.
+	 */
 	private int hexaDecimalToInteger(String hex) {
 		if(hex == null || hex.trim().isEmpty()) {
 			return 0;
@@ -128,7 +209,13 @@ public class HttpRequestHandler {
 	}
 	
 	
-	//this function only read CRLF ("\r\n") or 13 10 in bytes
+	/**
+	 * this function only read CRLF ("\r\n") or 13 10 in bytes
+	 *  and does nothing more.
+	 * @param inputStream
+	 *        |the inputStream on which the CRLF needs to be read of.
+	 * @throws Exception when the following input is not a CRLF
+	 */
 	private void readcrlf(InputStream inputStream) throws Exception {
 		int r = inputStream.read();
 		int n = inputStream.read();
@@ -137,16 +224,36 @@ public class HttpRequestHandler {
 		}
 	}
 	
+	/**
+	 * Returns the header line of the content-length header.
+	 * @param length
+	 *        | the length of the body.
+	 * @return a string representing the content-length header.
+	 * @throws IOException
+	 */
 	protected String writeContentLength(int length) throws IOException {
 		//return "Content-Length: " + length +"\n";
 		return "Content-Length: " + length;
 	}
 
+	/**
+	 * Returns the header line of the content-type header.
+	 * @param path
+	 *        | the type of which the content will be
+	 * @return the header line of the content-type header.
+	 * @throws IOException
+	 */
 	protected String writeContentType(Path path) throws IOException {
 		//return "Content-Type: " + getContentTypeFromPath(path) + "\n";
 		return "Content-Type: " + getContentTypeFromPath(path);
 	}
 
+	/**
+	 * Returns the content type of a given file.
+	 * @param path
+	 *        | the path of the file.
+	 * @return the content type of a given file.
+	 */
 	private String getContentTypeFromPath(Path path) {
 		String extension = "";
 		String fileName = path.getFileName().toString();
@@ -166,35 +273,66 @@ public class HttpRequestHandler {
 		}
 	}
 
+	/**
+	 * Returns the date line of the Date header.
+	 * @return a string representing the date header.
+	 * @throws IOException
+	 */
 	protected String writeDate() throws IOException {
 		//return "Date: " + java.time.format.DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now(ZoneOffset.UTC)) + "\n";
 		return "Date: " + java.time.format.DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now(ZoneOffset.UTC));
 	}
+	
 
+	/**
+	 * Writes a general 404 response to the ouputStream of the socket.
+	 * @param socket 
+	 *        | the socket on which a response needs to be written.
+	 * @throws IOException
+	 */
 	public void respond404(Socket socket) throws IOException {
 		System.out.println("not found");
-		String outString = "HTTP/1.1 404 Not Found\r\n\r\n";
+		//String outString = "HTTP/1.1 404 Not Found\r\n\r\n";
+		String outString = "HTTP/1.1 404 Not Found";
+		outString += writeDate() +"\n";
+		outString += writeContentLength(0);
+		outString +=  "\r\n\r\n";
 		OutputStream outputStream = socket.getOutputStream();
 		outputStream.write(outString.getBytes());
-		//outputStream.close();
-		
 	}
+	
+	/**
+	 * Writes a general 400 response to the outputStream of the socket.
+	 * @param socket
+	 *        | the socket on which a response needs to be written.
+	 * @throws IOException
+	 */
 	public void respond400(Socket socket) throws IOException {
 		System.out.println("bad request 400");
-		//String outString = "HTTP/1.1 400 Bad Request\n\n";
-		String outString = "HTTP/1.1 400 Bad Request\r\n\r\n";
+		//String outString = "HTTP/1.1 400 Bad Request\r\n\r\n";
+		String outString = "HTTP/1.1 400 Bad Request";
+		outString += writeDate() +"\n";
+		outString += writeContentLength(0);
+		outString +=  "\r\n\r\n";
 		OutputStream outputStream = socket.getOutputStream();
 		outputStream.write(outString.getBytes());
-		//outputStream.close();
 	}
 
+	/**
+	 * Writes a general 500 response to the outputStream of the socket.
+	 * @param socket 
+	 *        | the socket on which a response needs to be written.
+	 * @throws IOException
+	 */
 	public void respond500(Socket socket) throws IOException {
 		System.out.println("Server error");
-		String outString = "HTTP/1.1 500 Server Error\r\n\r\n";
+		//String outString = "HTTP/1.1 500 Server Error\r\n\r\n";
+		String outString = "HTTP/1.1 500 Server Error";
+		outString += writeDate() +"\n";
+		outString += writeContentLength(0);
+		outString +=  "\r\n\r\n";
 		OutputStream outputStream = socket.getOutputStream();
 		outputStream.write(outString.getBytes());
-		//outputStream.close();
-		
 	}
 
 }
